4d9a26feb73b68aa3b75dcf3a09669a6c94fd959
AndreBorja <andre.borja.miranda@gmail.com>
2022-11-24 10:40:07 -0800

    Changing Effector Area

--- Assets/Scripts/WindEffector.cs
+++ Assets/Scripts/WindEffector.cs
@@ -8,35 +8,31 @@ public class WindEffector : MonoBehaviour
     [SerializeField] private Transform _rayPoint;
     [SerializeField] private float _rayDistance;
     [SerializeField] private LayerMask _layerIndex;
-    private AreaEffector2D _wind;
     private BoxCollider2D _collider;
-    private Vector3 _area;
+    private Vector2 _originalArea;
 
     private void Start()
     {
-        _wind = GetComponent<AreaEffector2D>();
         _collider = GetComponent<BoxCollider2D>();
         _rayDistance = _collider.bounds.size.y;
+        _originalArea = _collider.size;
     }
 
 
     private void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(_rayPoint.position, Vector2.up, _rayDistance, _layerIndex);
-        Debug.DrawRay(_rayPoint.position, Vector2.up * _rayDistance);
         
         if (hit.collider != null)
         {
-            //_wind.enabled = false;
+            var halfRay = _rayDistance / 2;
             _collider.size = new Vector2(_collider.size.x, hit.point.y/_rayDistance);
-            _collider.offset = new Vector2(_collider.offset.x, (1 - hit.point.y) / 2 * -1);
-            //_collider.c
-            Debug.Log($"{_collider.size}");
-            //this is working, but the offset needs to change too
+            _collider.offset = new Vector2(_collider.offset.x, (1 - hit.point.y/_rayDistance) / 2 * -1);
         }
         else
         {
-            _wind.enabled = true;
+            _collider.size = _originalArea;
+            _collider.offset = Vector2.zero;
         }
     }
 }
