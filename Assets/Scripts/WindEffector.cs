5d643215155a7ff8502371f47ab6e8f2e89e3cf7
André Borja Miranda <andre.borja.miranda@gmail.com>
2022-11-24 00:06:49 -0800

    Testing Effector Size

--- Assets/Scripts/WindEffector.cs
+++ Assets/Scripts/WindEffector.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 
 public class WindEffector : MonoBehaviour
 {
-    [SerializeField] private AreaEffector2D _wind;
     [SerializeField] private Transform _rayPoint;
     [SerializeField] private float _rayDistance;
     [SerializeField] private LayerMask _layerIndex;
+    private AreaEffector2D _wind;
+    private BoxCollider2D _collider;
+    private Vector3 _area;
+
+    private void Start()
+    {
+        _wind = GetComponent<AreaEffector2D>();
+        _collider = GetComponent<BoxCollider2D>();
+    }
 
 
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(_rayPoint.position, Vector2.up, _rayDistance, _layerIndex);
+        RaycastHit2D hit = Physics2D.Raycast(_rayPoint.position, Vector2.up, _collider.bounds.size.y, _layerIndex);
         
         if (hit.collider != null)
         {
-            _wind.enabled = false;
+            //_wind.enabled = false;
+            _collider.size = new Vector2(_collider.size.x, hit.point.y);
+            Debug.Log($"{_collider.size}");
+            //this is working, but the offset needs to change too
         }
         else
         {
