bb783eb9d2981e978fb80839e8220d0a510bef41
André Borja Miranda <andre.borja.miranda@gmail.com>
2022-11-24 08:51:29 -0800

    Getting there (?)

--- Assets/Scripts/WindEffector.cs
+++ Assets/Scripts/WindEffector.cs
@@ -16,17 +16,21 @@ public class WindEffector : MonoBehaviour
     {
         _wind = GetComponent<AreaEffector2D>();
         _collider = GetComponent<BoxCollider2D>();
+        _rayDistance = _collider.bounds.size.y;
     }
 
 
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(_rayPoint.position, Vector2.up, _collider.bounds.size.y, _layerIndex);
+        RaycastHit2D hit = Physics2D.Raycast(_rayPoint.position, Vector2.up, _rayDistance, _layerIndex);
+        Debug.DrawRay(_rayPoint.position, Vector2.up * _rayDistance);
         
         if (hit.collider != null)
         {
             //_wind.enabled = false;
             _collider.size = new Vector2(_collider.size.x, hit.point.y);
+            _collider.offset = new Vector2(_collider.offset.x, (1 - hit.point.y) / 2 * -1);
+            //_collider.c
             Debug.Log($"{_collider.size}");
             //this is working, but the offset needs to change too
         }
